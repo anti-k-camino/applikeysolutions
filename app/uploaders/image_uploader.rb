c3f43ac1b9975bd7aff9a8b7070cabac95b49ba4
Anti-k <romanslyzebniy@gmail.com>
2016-10-21 13:46:31 +0300

    uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,6 +15,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
